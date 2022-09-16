3bf948f37cfeb90ee193f47077e39ec1951e6ed0
Orleydovsky <orleydovsky@gmail.com>
2022-09-15 22:47:11 -0500

    build: :heavy_plus_sign: Implement ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    project: ['tsconfig.json'],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/jsx-indent': ['error', 2]
+  }
+}
