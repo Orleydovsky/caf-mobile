95eb4e2b4d9fb062a794c80117416d680ef7da67
Orleydovsky <orleydovsky@gmail.com>
2022-09-22 20:11:21 -0500

    feat: :sparkles: Implement StatusBar

--- App.tsx
+++ App.tsx
@@ -3,16 +3,19 @@ import { useFonts } from 'expo-font'
 import { Fragment } from 'react'
 import HomeMobile from './screens/home-mobile'
 import Brochure from './features/brochure/brochure'
+import { StatusBar } from 'react-native'
+import { darkBlue } from './constants/colors'
 
 export default function App () {
-  const { cafs } = useCafs()
   const [fontsLoaded] = useFonts({
     nunitoRegular: require('./assets/fonts/Nunito-Regular.ttf'),
     nunitoBold: require('./assets/fonts/Nunito-Bold.ttf')
   })
   if (!fontsLoaded) return null
+
   return (
     <Fragment>
+      <StatusBar hidden={false} backgroundColor={darkBlue}/>
       <NativeRouter>
         <Routes>
           <Route path='/' element={<HomeMobile/>}/>
